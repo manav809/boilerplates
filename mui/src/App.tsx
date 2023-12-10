12dd932341daa16901f980de9a28d2775db1244a
manav809 <manavpatel6978@gmail.com>
2023-12-10 12:43:44 -0500

    Add Typography and Iconography

--- mui/src/App.tsx
+++ mui/src/App.tsx
@@ -1,11 +1,47 @@
+import { Button, Container, Fab, Stack, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import AddIcon from "@mui/icons-material/Add";
+import React from "react";
+
+const StyledButton = styled(Button)(() => ({
+  backgroundColor: "#f6ccff",
+}));
 
 function App() {
   return (
-    <div>
-      <header>
-          Header
-      </header>
-    </div>
+    <Container>
+      <Stack spacing={2}>
+        <Stack direction="row" spacing={2} sx={{ alignItems: "end" }}>
+          <Typography variant="h1">Hello</Typography>
+          <Typography variant="h2">Hello</Typography>
+          <Typography variant="subtitle1">Hello</Typography>
+          <Typography variant="subtitle2">Hello</Typography>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained">Contained primary button</Button>
+          <Button variant="contained" color="success">
+            Secondary Button
+          </Button>
+          <Button variant="outlined" color="primary" disableRipple>
+            Contained outlined button
+          </Button>
+          <StyledButton variant="outlined" color="secondary">
+            Styled button
+          </StyledButton>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Fab color="primary">
+            <AddIcon />
+          </Fab>
+          <Fab color="primary" size="medium">
+            <AddIcon />
+          </Fab>
+          <Fab color="primary" size="small">
+            <AddIcon />
+          </Fab>
+        </Stack>
+      </Stack>
+    </Container>
   );
 }
 
--- mui/src/index.tsx
+++ mui/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import theme from './theme'
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
-
