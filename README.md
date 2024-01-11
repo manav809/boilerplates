089726534507126d56a04c8e8241de5dcd7cacf3
manav809 <manavpatel6978@gmail.com>
2024-01-10 19:07:57 -0500

    Add Chaining

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Boilerplates: Material UI, Tailwind, SQL Scripts, 
 
-[![My Skills](https://skillicons.dev/icons?i=materialui,tailwind,nodejs,fastapi,mysql)](https://manavpatel.net)
+[![My Skills](https://skillicons.dev/icons?i=materialui,tailwind,nodejs,mysql)](https://manavpatel.net)
--- server-mvc/index.ts
+++ server-mvc/index.ts
@@ -1,12 +1,42 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import listEndpoints from "express-list-endpoints";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(express.json()); //middleware that allows for us to use json
+
+// app.get("/", (req: Request, res: Response) => {
+//   //res.send("Express + TypeScript Server");
+//   // res.json({
+//   //   success: true,
+//   // });
+//   return res.redirect("http://google.com");
+// });
+
+// app.post("/api/data", (req: Request, res: Response) => {
+//   console.log(req.body);
+//   return res.sendStatus(200);
+// });
+
+// app.all("/api/all", (req, res) => {
+//   return res.sendStatus(200);
+// });
+
+app
+  .route("/")
+  .get((req: Request, res: Response) => {
+    return res.send("You made a get request");
+  })
+  .post((req: Request, res: Response) => {
+    return res.send("You made a post request");
+  })
+  .put((req: Request, res: Response) => {
+    return res.send("You made a put request");
+  });
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+console.table(listEndpoints(app));
