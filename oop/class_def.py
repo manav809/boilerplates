e9d3b5b588fc7fbd8dd5af7f73b3bd72710717bc
manav809 <manavpatel6978@gmail.com>
2024-03-22 10:05:23 -0400

    Add Basic Python Class

---
+++ oop/class_def.py
@@ -0,0 +1,11 @@
+
+class Class:
+    def __init__(self):
+        pass
+
+class Student(Class):
+    def __init__(self, type):
+        self.type = type
+
+    def type(self):
+        return "blah"
