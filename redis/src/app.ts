e9b2a841f7db6b2f4b804ea05d58b9780f51710c
manav809 <manavpatel6978@gmail.com>
2025-07-19 12:24:39 -0400

    Simple Redis Example

--- redis/src/app.ts
+++ redis/src/app.ts
@@ -1,18 +1,37 @@
 import express from "express";
 import cors from "cors";
 import axios from "axios";
+import * as redis from "redis";
 
 const app = express();
 
 app.use(cors());
 const port = 3000;
+const client = redis.createClient();
+
+const DEFAULT_EXPIRATION = 3600;
+client.connect().catch(console.error);
 
 app.get("/photos", async (req, res) => {
   const albumId = req.query.albumId;
+
+  const cachedPhotos = await client.get("photos");
+
+  if (cachedPhotos != null) {
+    const dataString =
+      typeof cachedPhotos === "string"
+        ? cachedPhotos
+        : cachedPhotos.toString("utf-8");
+    console.log("Cache Hit")
+    return res.json(JSON.parse(dataString));
+  }
   const { data } = await axios.get(
     "https://jsonplaceholder.typicode.com/photos",
     { params: { albumId } }
   );
+  console.log('Cache Miss')
+  client.SETEX("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
+
   res.json(data);
 });
 
